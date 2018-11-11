3e8ac97affba91101c3432555689396a3a8f6689
Karthikeyan <sakthikeyan@gmail.com>
2018-11-11 17:25:58 +0530

    Second commit

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
 
 
 @Component({
@@ -9,47 +9,59 @@ import { FormGroup, FormControl } from '@angular/forms'
 })
 export class CreateEmployeeComponent implements OnInit {
   employeeForm: FormGroup;
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.employeeForm = new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl(),
-      // Create Skill Fomr Group
-      skills: new FormGroup({
-        skillName: new FormControl(),
-        experienceInYear: new FormControl(),
-        proficiency: new FormControl()
+    // this.employeeForm = new FormGroup({
+    //   fullName: new FormControl(),
+    //   email: new  FormControl(),
+    //   // Create Skill Fomr Group
+    //   skills: new FormGroup({
+    //     skillName: new FormControl(),
+    //     experienceInYear: new FormControl(),
+    //     proficiency: new FormControl()
+    //   })
+    // });
+
+    //Using Form Builder.
+
+    this.employeeForm = this.fb.group({
+      fullName: [''],
+      email: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experienceInYear: [''],
+        proficiency: ['advanced']
       })
-    });
+    })
   }
 
   onLoadDataClick(): void {
     //use setVallue to update all form control.
-    // this.employeeForm.setValue(
-    //   {
-    //     fullName: "Karthikeyan Sakthi",
-    //     email: "sakthikeyan@gmail.com",
-    //     skills: {
-    //       skillName: "ASP.NET MVC",
-    //       experienceInYear: "6",
-    //       proficiency: "advanced"
-    //     }
-    //   }
-    // )
 
-    //use patchValue to update sub set of controlsin the form.
-    this.employeeForm.patchValue(
+    this.employeeForm.setValue(
       {
         fullName: "Karthikeyan Sakthi",
         email: "sakthikeyan@gmail.com",
-        // skills: {
-        //   skillName: "ASP.NET MVC",
-        //   experienceInYear: "6",
-        //   proficiency: "advanced"
-        // }
+        skills: {
+          skillName: "ASP.NET MVC",
+          experienceInYear: "6",
+          proficiency: "advanced"
+        }
       }
     )
+    //use patchValue to update sub set of controlsin the form.
+    // this.employeeForm.patchValue(
+    //   {
+    //     fullName: "Karthikeyan Sakthi",
+    //     email: "sakthikeyan@gmail.com",
+    //     // skills: {
+    //     //   skillName: "ASP.NET MVC",
+    //     //   experienceInYear: "6",
+    //     //   proficiency: "advanced"
+    //     // }
+    //   }
+    // )
   }
   onSubmit(): void {
     // console.log(this.employeeForm.controls.fullName.value)
