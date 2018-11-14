2435b6bf8139e496e3e6b3a0c0fe65a700d5f137
Karthikeyan <sakthikeyan@gmail.com>
2018-11-15 00:12:30 +0530

    Angular reactive form custom validator with parameter

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -18,7 +18,7 @@ export class CreateEmployeeComponent implements OnInit {
     },
     'email': {
       'required': 'Email is required.',
-      'emailDomain': 'Email domian should be pragimtech.com'
+      'emailDomain': 'Email domian should be gmail.com'
     },
     'phone': {
       'required': 'Phone is required.'
@@ -62,7 +62,7 @@ export class CreateEmployeeComponent implements OnInit {
     this.employeeForm = this.fb.group({
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
       contactPreference: ['email'],
-      email: ['', [Validators.required, emailDomain]],
+      email: ['', [Validators.required, emailDomain('gmail.com')]],
       phone: [''],
       skills: this.fb.group({
         skillName: ['', Validators.required],
@@ -186,12 +186,26 @@ export class CreateEmployeeComponent implements OnInit {
   }
 }
 
-function emailDomain(control: AbstractControl): { [key: string]: any } | null {
-  const email: string = control.value;
-  const domain = email.substring(email.lastIndexOf('@') + 1);
-  if (email === '' || domain.toLowerCase() === 'pragimtech.com') {
-    return null;
-  } else {
-    return { 'emailDomain': true };
-  }
+// function emailDomain(control: AbstractControl): { [key: string]: any } | null {
+//   const email: string = control.value;
+//   const domain = email.substring(email.lastIndexOf('@') + 1);
+//   if (email === '' || domain.toLowerCase() === 'pragimtech.com') {
+//     return null;
+//   } else {
+//     return { 'emailDomain': true };
+//   }
+//}
+
+
+//Angular reactive form custom validator with parameter
+function emailDomain(domainName: string) {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const email: string = control.value;
+    const domain = email.substring(email.lastIndexOf('@') + 1);
+    if (email === '' || domain.toLowerCase() === domainName.toLowerCase()) {
+      return null;
+    } else {
+      return { 'emailDomain': true };
+    }
+  };
 }
